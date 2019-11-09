cb8422516ce0d75065d2a2dc426e1805f8f3de52
inkanusmaster <furymistrz@gmail.com>
2019-11-09 19:17:59 +0100

    Zaczynamy news reader

---
+++ app/src/main/java/com/example/section7_appnewsreader/WebReader.java
@@ -0,0 +1,14 @@
+package com.example.section7_appnewsreader;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebReader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reader_web);
+    }
+}
