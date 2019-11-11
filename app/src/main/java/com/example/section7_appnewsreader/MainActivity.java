7f5ef6618087f4b71e586875e7dbddd7448e6546
inkanusmaster <furymistrz@gmail.com>
2019-11-11 22:20:53 +0100

    PROGRES! Mam dane w bazie. Teraz zrób arraylist z tytułami jako arrayadapter No i po kliknięciu w tytuł, niech otworzy się odpowiedni URL

--- app/src/main/java/com/example/section7_appnewsreader/MainActivity.java
+++ app/src/main/java/com/example/section7_appnewsreader/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.section7_appnewsreader;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
 import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -28,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     String[] id = new String[10];
     String[] urlArticleJSON = new String[10];
     HashMap<String, String> titleUrlMap = new HashMap<>();
-    String titleOfArticle, urlOfArticle;
+    SQLiteDatabase newsDatabase;
 
     @SuppressLint("StaticFieldLeak")
     public class DownloadTask extends AsyncTask<String, Void, HashMap<String, String>> {
@@ -36,10 +34,8 @@ public class MainActivity extends AppCompatActivity {
         protected HashMap<String, String> doInBackground(String... urls) {
             StringBuilder resultArticleID = new StringBuilder();
             StringBuilder resultNewsJSON = new StringBuilder();
-
             URL url, url2;
             HttpURLConnection urlConnection, urlConnection2;
-
 
             try {
                 url = new URL(urls[0]);
@@ -57,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
                 for (int i = 0; i < 10; i++) {
                     id[i] = jsonURLArticlesArray.getString(i);
-//                        System.out.println("ID" + i + " : " + id[i]);
 
                     Pattern p = Pattern.compile("item/(.*?)\\.json?");
                     Matcher m = p.matcher(urlNews);
@@ -65,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
 
                         urlNews = urlNews.replace(m.group(1), id[i]);
                         urlArticleJSON[i] = urlNews;
-                        System.out.println("ID" + i + " : " + urlArticleJSON[i]);
 
                         url2 = new URL(urlArticleJSON[i]);
                         urlConnection2 = (HttpURLConnection) url2.openConnection();
@@ -88,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 Pattern pattern = Pattern.compile("\"title\" : \"(.*?)\",\n" + " .*\n" + "  \"url\" : \"(.*?)\"");
                 Matcher matcher = pattern.matcher(resultNewsJSON);
                 while (matcher.find()) {
-                    titleUrlMap.put(matcher.group(1),matcher.group(2));
+                    titleUrlMap.put(matcher.group(1), matcher.group(2));
                 }
 
             } catch (Exception e) {
@@ -96,16 +90,40 @@ public class MainActivity extends AppCompatActivity {
             }
 
             return titleUrlMap;
-
         }
 
-        protected void onPostExecute(HashMap<String, String> s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(HashMap<String, String> hashMap) {
+            super.onPostExecute(hashMap);
 
-            System.out.println("WYNICZEK\n" + s);
+            try {
+                Set entries = titleUrlMap.entrySet();
+                Iterator iterator = entries.iterator();
+                ContentValues contentValues = new ContentValues();
+                while (iterator.hasNext()) {
+                    Map.Entry entry = (Map.Entry) iterator.next();
+                    Object title = entry.getKey();
+                    Object url = entry.getValue();
+                    contentValues.put("title", String.valueOf(title));
+                    contentValues.put("url", String.valueOf(url));
+                    newsDatabase.insert("news", null, contentValues);
+                }
+
+                @SuppressLint("Recycle") Cursor c = newsDatabase.rawQuery("SELECT * FROM news", null);
+                int titleIndex = c.getColumnIndex("title");
+                int urlIndex = c.getColumnIndex("url");
+                c.moveToFirst();
+                while (!c.isAfterLast()) {
+                    System.out.println("TITLE " + c.getString(titleIndex));
+                    System.out.println("URL " + c.getString(urlIndex));
+                    System.out.println(" ");
+                    c.moveToNext();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
-
 
     public void downloadNews() {
         DownloadTask task = new DownloadTask();
@@ -116,10 +134,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void createDatabase() {
+        newsDatabase = this.openOrCreateDatabase("News", MODE_PRIVATE, null);
+        newsDatabase.execSQL("CREATE TABLE IF NOT EXISTS news (title VARCHAR, url VARCHAR)");
+        newsDatabase.execSQL("DELETE FROM news");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createDatabase();
         downloadNews();
     }
 }
