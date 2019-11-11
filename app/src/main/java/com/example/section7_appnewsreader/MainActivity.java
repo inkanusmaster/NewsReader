6ae4ee4b1326df5f3973974815b60e4a29c009f7
inkanusmaster <furymistrz@gmail.com>
2019-11-11 20:27:07 +0100

    CORAZ LEPIEJ. Przesyłam do onpostexecute hashmap. Jako Key zawiera tytuł, jako Value zawiera URL. Trzeba teraz zrobić bazę do której dodam key i value.

--- app/src/main/java/com/example/section7_appnewsreader/MainActivity.java
+++ app/src/main/java/com/example/section7_appnewsreader/MainActivity.java
@@ -11,10 +11,13 @@ import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,14 +27,19 @@ public class MainActivity extends AppCompatActivity {
     String urlNews = "https://hacker-news.firebaseio.com/v0/item/ENTERIDHERE.json?print=pretty";
     String[] id = new String[10];
     String[] urlArticleJSON = new String[10];
+    HashMap<String, String> titleUrlMap = new HashMap<>();
+    String titleOfArticle, urlOfArticle;
 
     @SuppressLint("StaticFieldLeak")
-    public class DownloadTask extends AsyncTask<String, Void, String> {
+    public class DownloadTask extends AsyncTask<String, Void, HashMap<String, String>> {
         @Override
-        protected String doInBackground(String... urls) {
-            StringBuilder result = new StringBuilder();
-            URL url;
-            HttpURLConnection urlConnection;
+        protected HashMap<String, String> doInBackground(String... urls) {
+            StringBuilder resultArticleID = new StringBuilder();
+            StringBuilder resultNewsJSON = new StringBuilder();
+
+            URL url, url2;
+            HttpURLConnection urlConnection, urlConnection2;
+
 
             try {
                 url = new URL(urls[0]);
@@ -41,41 +49,63 @@ public class MainActivity extends AppCompatActivity {
                 int data = reader.read();
                 while (data != -1) {
                     char current = (char) data;
-                    result.append(current);
+                    resultArticleID.append(current);
                     data = reader.read();
                 }
 
-                return result.toString();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
+                JSONArray jsonURLArticlesArray = new JSONArray(resultArticleID.toString());
 
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            try {
-                JSONArray jsonURLArticlesArray = new JSONArray(s);
                 for (int i = 0; i < 10; i++) {
                     id[i] = jsonURLArticlesArray.getString(i);
-                    System.out.println("ID" + i + " : " + id[i]);
+//                        System.out.println("ID" + i + " : " + id[i]);
 
                     Pattern p = Pattern.compile("item/(.*?)\\.json?");
                     Matcher m = p.matcher(urlNews);
                     if (m.find()) {
+
                         urlNews = urlNews.replace(m.group(1), id[i]);
                         urlArticleJSON[i] = urlNews;
                         System.out.println("ID" + i + " : " + urlArticleJSON[i]);
+
+                        url2 = new URL(urlArticleJSON[i]);
+                        urlConnection2 = (HttpURLConnection) url2.openConnection();
+                        InputStream input = urlConnection2.getInputStream();
+                        InputStreamReader r = new InputStreamReader(input);
+                        int dat = r.read();
+                        while (dat != -1) {
+                            char cur = (char) dat;
+                            resultNewsJSON.append(cur);
+                            dat = r.read();
+                        }
                     }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            try {
+                Pattern pattern = Pattern.compile("\"title\" : \"(.*?)\",\n" + " .*\n" + "  \"url\" : \"(.*?)\"");
+                Matcher matcher = pattern.matcher(resultNewsJSON);
+                while (matcher.find()) {
+                    titleUrlMap.put(matcher.group(1),matcher.group(2));
                 }
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
+            return titleUrlMap;
+
         }
 
+        protected void onPostExecute(HashMap<String, String> s) {
+            super.onPostExecute(s);
+
+            System.out.println("WYNICZEK\n" + s);
+        }
     }
+
 
     public void downloadNews() {
         DownloadTask task = new DownloadTask();
@@ -85,22 +115,6 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
-//    public void downloadNews() {
-//        System.out.println("TEST ID[0] w downloadnewsaaa " + Arrays.toString(id));
-//        DownloadTask task = new DownloadTask();
-//        try {
-//            Pattern p = Pattern.compile("item/(.*?)\\.json?");
-//            Matcher m = p.matcher(urlNews);
-//            if (m.find()) {
-//                urlNews = urlNews.replace(m.group(1), id[0]);
-//                task.execute(urlNews).get();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
